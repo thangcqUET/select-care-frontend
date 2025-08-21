6c0d70869f063f0b7ff800ccce9e60502e8cd593
thangcqUET <thangcq98@gmail.com>
2025-08-22 01:42:33 +0700

    add sign up with magic link (only function without UI/UX)

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,11 +1,20 @@
-// auth.ts
 import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
-export const { auth, handlers } = NextAuth({ providers: [GitHub] })
+import Resend from "next-auth/providers/resend"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "@/app/lib/prisma"
+import jwt from "jsonwebtoken"
 
-// middleware.ts
-export { auth as middleware } from "@/auth"
-
-// app/api/auth/[...nextauth]/route.ts
-import { handlers } from "@/auth"
-export const { GET, POST } = handlers
+export const { handlers, auth, signIn, signOut } = NextAuth({
+    adapter: PrismaAdapter(prisma),
+    providers: [Resend({
+        from: "no-reply@tononla.com",
+        // generateVerificationToken() {
+        //     return jwt.sign({ code: crypto.randomUUID() }, process.env.JWT_SECRET!, { expiresIn: "1h" })
+        // },
+    })],
+    secret: process.env.AUTH_SECRET,
+    session: {
+        strategy: "jwt",
+        maxAge: 60 * 60 * 24 * 90, // 90 days
+    },
+})
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import { SignIn } from "./components/sign-in";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <button>Sign-up</button>
+      <SignIn></SignIn>
       <button>Payment</button>
     </div>
   );
